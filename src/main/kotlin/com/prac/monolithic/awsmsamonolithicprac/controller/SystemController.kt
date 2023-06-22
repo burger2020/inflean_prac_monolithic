abaaaaa1930a021394e6945b45373fade9eada2d
burger2020 <blac2013@gmail.com>
2023-06-22 10:58:01 +0900

    버전 추가

--- src/main/kotlin/com/prac/monolithic/awsmsamonolithicprac/controller/SystemController.kt
+++ src/main/kotlin/com/prac/monolithic/awsmsamonolithicprac/controller/SystemController.kt
@@ -1,8 +1,10 @@
 package com.prac.monolithic.awsmsamonolithicprac.controller
 
+import com.sun.management.OperatingSystemMXBean
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.lang.management.ManagementFactory
 import java.time.LocalDateTime
 
 @RestController
@@ -13,7 +15,15 @@ class SystemController {
         mapOf(
             "status" to "OK",
             "message" to "System is healthy",
-            "timestamp" to LocalDateTime.now()
+            "timestamp" to LocalDateTime.now(),
+            "cpuUsage" to getCurrentCpuUsage(),
+            "branch" to "2_monolithic_cloud",
         )
     )
+
+
+    fun getCurrentCpuUsage(): Double {
+        val osBean = ManagementFactory.getOperatingSystemMXBean() as OperatingSystemMXBean
+        return osBean.cpuLoad * 100
+    }
 }
