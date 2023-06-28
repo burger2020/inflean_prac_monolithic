2a22ecb4e32f6453632720f151514b60fce049ec
burger2020 <blac2013@gmail.com>
2023-06-28 11:17:35 +0900

    s3업로드 완료

--- src/main/kotlin/com/prac/monolithic/awsmsamonolithicprac/service/S3Service.kt
+++ src/main/kotlin/com/prac/monolithic/awsmsamonolithicprac/service/S3Service.kt
@@ -2,18 +2,26 @@ package com.prac.monolithic.awsmsamonolithicprac.service
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
+
 
 @Service
 class S3Service(
     @Value("\${cloud.aws.region.static}") private val region: String,
-    @Value("\${cloud.aws.s3.bucket}") private val bucket: String
+    @Value("\${cloud.aws.s3.bucket}") private val bucket: String,
+    @Value("\${cloud.aws.credentials.accesskey}") private val accessKey: String,
+    @Value("\${cloud.aws.credentials.secretkey}") private val secretKey: String
 ) {
 
+    private var awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey)
     private val s3: S3Client = S3Client.builder()
+        .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
         .region(Region.of(region)) // Set your region
         .build()
 
@@ -22,6 +30,7 @@ class S3Service(
             PutObjectRequest.builder()
                 .bucket(bucket)
                 .key(key)
+                .contentType("image/jpeg")
                 .build(),
             RequestBody.fromBytes(bytes)
         )
