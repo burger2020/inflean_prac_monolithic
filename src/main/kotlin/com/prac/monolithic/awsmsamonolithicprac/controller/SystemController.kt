578de77a9b0ebf88fb3b0bc4cbf7e1d73a004386
burger2020 <blac2013@gmail.com>
2023-06-28 15:51:40 +0900

    yam 수정

--- src/main/kotlin/com/prac/monolithic/awsmsamonolithicprac/controller/SystemController.kt
+++ src/main/kotlin/com/prac/monolithic/awsmsamonolithicprac/controller/SystemController.kt
@@ -25,8 +25,7 @@ class SystemController(
             "ipAddress" to InetAddress.getLocalHost().hostAddress,
             "timestamp" to LocalDateTime.now(),
             "cpuUsage" to getCurrentCpuUsage(),
-            "branch" to "4_monolithic_rds",
-            "connection url" to systemService.getDataConnectionUrl()
+            "branch" to "5_1_monolithic_s3"
         )
     )
 
@@ -34,35 +33,5 @@ class SystemController(
     fun getCurrentCpuUsage(): Double {
         val osBean = ManagementFactory.getOperatingSystemMXBean() as OperatingSystemMXBean
         return osBean.cpuLoad * 100
-    }
-
-    @GetMapping("/cpu-load")
-    fun cpuLoadTest(): String {
-        val startTime = System.currentTimeMillis()
-        val primes = generatePrimes(50000) // 계산 집약적인 작업
-        val endTime = System.currentTimeMillis()
-
-        return "Found ${primes.size} primes in ${endTime - startTime} milliseconds"
-    }
-
-    // 소수 생성 함수 (CPU 사용량 증가)
-    private fun generatePrimes(limit: Int): List<Int> {
-        val primes = mutableListOf(2)
-        var nextPrime = 3
-        while (primes.size < limit) {
-            var isPrime = true
-            val sqrt = sqrt(nextPrime.toDouble()).toInt()
-            for (j in 2..sqrt) {
-                if (nextPrime % j == 0) {
-                    isPrime = false
-                    break
-                }
-            }
-            if (isPrime) {
-                primes.add(nextPrime)
-            }
-            nextPrime += 2
-        }
-        return primes
     }
 }
